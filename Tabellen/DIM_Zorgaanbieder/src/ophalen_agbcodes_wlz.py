1c9a36ca07301e6a7ba7c8bd72a5f1a5ef0e1c70
Jesper van Beemdelust <jespervanbeemdelust@hotmail.com>
2023-12-28 21:06:14 +0100

    Ophalen AGB codes script afgerond

--- Tabellen/DIM_Zorgaanbieder/src/ophalen_agbcodes_wlz.py
+++ Tabellen/DIM_Zorgaanbieder/src/ophalen_agbcodes_wlz.py
@@ -1,4 +1,6 @@
+from config import BASE_PATH, USER_AGENT
 import requests
+import os
 import json
 
 def extract(url: str) -> json:
@@ -7,17 +9,18 @@ def extract(url: str) -> json:
     De data die opgehaald wordt is de zorgaanbiederslijst. De gehele lijst wordt meegenomen, maar niet de historie van individuele records.
 
     Args:
-        url (string): 
+        url (string): URL van de API.
 
     Raises:
-        Exception: _description_
+        Exception: API request failed with the given status code.
 
     Returns:
-        _type_: _description_
+        json: JSON-gegevens van de API-respons.
     """
+
     headers = {
     'Accept': 'application/json',
-    'User-Agent': 'Wachtlijsten dashboard (jespervanbeemdelust@hotmail.com)'
+    'User-Agent': USER_AGENT
     }
 
     response = requests.request("GET", url, headers=headers)
@@ -26,21 +29,26 @@ def extract(url: str) -> json:
         print(response.text)
         return response.json()
     else:
-        raise Exception
-        print(f"API kan niet bevraagd worden, de volgende statuscode is ontvangen {response.status_code}")
+        raise Exception(f"API request failed. Status code: {response.status_code}. Response: {response.text}")
 
-def load_raw(response_data, file_path):
 
+def load_raw(response_data: json, file_path: str) -> None:
+    """
+    Slaat de ruwe JSON-gegevens op in een bestand.
+
+    Args:
+        response_data (json): JSON-gegevens om op te slaan.
+        file_path (str): Bestandspad waarin de gegevens moeten worden opgeslagen.
+    """
     with open(file_path, 'w') as json_file:
         json.dump(response_data, json_file, indent=2)
 
 def main():
-
     # Url van de API
     base_url = "https://modules.istandaarden.nl/tabelbeheer/public/api/zorgaanbieders?showActiveOnly=false"
 
     # Locatie van de json gegevens confrom standaard: brons/raw
-    raw_locatie = 'C:\Code\dashboard_projects\wlz_wachtlijsten\Tabellen\DIM_Zorgaanbieder\Raw\Zorgaanbieders_istandaarden.json'
+    raw_locatie = os.path.join(BASE_PATH, 'Tabellen', 'DIM_Zorgaanbieder', 'Raw', 'Zorgaanbieders_istandaarden.json')
     json_data = extract(base_url)
 
     load_raw(json_data, raw_locatie)
